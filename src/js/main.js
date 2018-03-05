7122680ac565899465ef39b65eb1369b31e24a03
Kathleen Levi <levi.kathleen@gmail.com>
2018-03-04 17:04:17 -0800

    begin slideshow template

--- src/js/main.js
+++ src/js/main.js
@@ -16,40 +16,25 @@ app.config(function($routeProvider) {
     })
 	.when("/drop1", {
         templateUrl : "partials/drop1.htm",
-        controller : 'main as ctrl'
+        controller : 'drop1 as ctrl'
     })
 	.when("/drop2", {
         templateUrl : "partials/drop2.htm",
-        controller : 'main as ctrl'
+        controller : 'drop2 as ctrl'
     })
 });
 
-app.controller('main', function($scope, $http) {
+app.controller('drop1', function($scope, $http) {
 	var vm = this,
 		img = ["default1.jpg", "default2.jpg", "default3.jpg", "default4.jpg", "default1.jpg", "default2.jpg", "default3.jpg", "default4.jpg", "default1.jpg", "default2.jpg", "default3.jpg", "default4.jpg"];
 
 	$(document).ready(function() { 
 		//finds images and gives a path based on page
 		vm.images = convertImagePath(img);
-
+		
 		//Update the page
 		$scope.$apply();
 	});
-
-
-	//converts a ton of images to the correct path
-	function convertImagePath(img){
-		var imgPath = []
-		angular.forEach(img, function(item, idx){
-			imgPath.push({"name":item, "path":getImagePath(item)});
-		})
-		return imgPath;
-	};
-
-	//get image path
-	function getImagePath(imageName){
-		return "img/" + imageName;	
-	};
 	
 	vm.openModal = function(image) {
 		vm.title = image.name;
@@ -57,4 +42,34 @@ app.controller('main', function($scope, $http) {
 	};
 });
 
+app.controller('drop2', function($scope, $http) {
+	var vm = this,
+		img = ["default1.jpg", "default2.jpg", "default3.jpg", "default4.jpg"];
+
+	$(document).ready(function() { 
+		//finds images and gives a path based on page
+		vm.images = convertImagePath(img);
+		
+		//Update the page
+		$scope.$apply();
+	});
 	
+	vm.openModal = function(image) {
+		vm.title = image.name;
+		vm.modalImg = image.path;
+	};
+});
+
+//converts a ton of images to the correct path
+function convertImagePath(img){
+	var imgPath = []
+	angular.forEach(img, function(item, idx){
+		imgPath.push({"name":item, "path":getImagePath(item)});
+	})
+	return imgPath;
+};
+
+//get image path
+function getImagePath(imageName){
+	return "img/" + imageName;	
+};

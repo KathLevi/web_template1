e0c0cc3d59675a0180afeaf25343af30438aa432
Kathleen Levi <levi.kathleen@gmail.com>
2018-03-25 17:24:31 -0700

    Added more template pages

--- src/js/main.js
+++ src/js/main.js
@@ -6,25 +6,33 @@ app.config(function($routeProvider) {
         templateUrl : "partials/home.htm",
         controller : 'main as ctrl'
     })
-    .when("/page1", {
-        templateUrl : "partials/page1.htm",
+    .when("/portfolio", {
+        templateUrl : "partials/portfolio.htm",
         controller : 'main as ctrl'
     })
-    .when("/page2", {
-        templateUrl : "partials/page2.htm",
-        controller : 'page2 as ctrl'
+	.when("/portfolio1", {
+        templateUrl : "partials/portfolio1.htm",
+        controller : 'portfolio1 as ctrl'
     })
-	.when("/drop1", {
-        templateUrl : "partials/drop1.htm",
-        controller : 'drop1 as ctrl'
-    })
-	.when("/drop2", {
-        templateUrl : "partials/drop2.htm",
+	.when("/portfolio2", {
+        templateUrl : "partials/portfolio2.htm",
+        controller : 'portfolio2 as ctrl'
+	})
+	.when("/team", {
+        templateUrl : "partials/team.htm",
+        controller : 'team as ctrl'
+	})
+	.when("/contact", {
+        templateUrl : "partials/contact.htm",
+        controller : 'main as ctrl'
+	})
+	.when("/signIn", {
+        templateUrl : "partials/signIn.htm",
         controller : 'main as ctrl'
     })
 });
 
-app.controller('drop1', function($scope, $http) {
+app.controller('portfolio1', function($scope, $http) {
 	var vm = this,
 		img = ["default1.jpg", "default2.jpg", "default3.jpg", "default4.jpg", "default1.jpg", "default2.jpg", "default3.jpg", "default4.jpg", "default1.jpg", "default2.jpg", "default3.jpg", "default4.jpg"];
 
@@ -37,12 +45,48 @@ app.controller('drop1', function($scope, $http) {
 	};
 });
 
-app.controller('page2', function($scope, $http) {
+app.controller('portfolio2', function($scope, $http) {
 	var vm = this,
 		img = ["default1.jpg", "default1.jpg", "default1.jpg", "default1.jpg"];
 
 	//finds images and gives a path based on page
 	vm.images = convertImagePath(img);
+});
+
+app.controller('team', function($scope, $http) {
+    var vm = this,
+        team = [
+            {
+                name: "Person A",
+                path: "img/default3.jpg",
+                desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            },
+            {
+                name: "Person B",
+                path: "img/default3.jpg",
+                desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            },
+            {
+                name: "Person C",
+                path: "img/default3.jpg",
+                desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            }
+        ];
+
+	//finds images and gives a path based on page
+    vm.team = team;
+
+    vm.dropModal = function(image) {
+        if (vm.teamName == undefined) {
+            vm.teamName = image.name;
+            vm.teamDesc = image.desc;
+        }
+        else {
+            vm.teamName = null;
+            vm.teamDesc = null;
+        }
+        
+    };
 });
 
 //converts a ton of images to the correct path
